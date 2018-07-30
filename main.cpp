bd0503ac2321ce2ae8b6b3752595e87de2ac38ef
oklodhi <okhanlodhi@gmail.com>
2018-07-29 23:17:16 -0500

    Convert brightness values to ASCII characters

--- main.cpp
+++ main.cpp
@@ -7,28 +7,28 @@ using namespace cimg_library;
 
 int main() {
 
-    // open "asciidog.jpg" as image object
+    /* Open "asciidog.jpg" as image object */
     CImg <unsigned char> image("asciidog.jpg");
 
-    // get image dimensions and color channels
+    /* Get image dimensions and color channels */
     int imageWidth = image.width();
     int imageHeight = image.height();
     int imageSpec = image.spectrum();
 
-    // check for valid image & print dimensions
+    /* Check for valid image & print dimensions */
     if (image != NULL) {
         cout << "Successfully loaded image!" << endl;
-        cout << "Image Size: " << imageWidth << " x " << imageHeight << " \t" << imageSpec << " channels" << endl << endl;
+        cout << "Image Size: " << imageWidth << " x " << imageHeight << " \t" << imageSpec << " channels" << endl;
     } else {
         cout << "Error: invalid image!" << endl;
     }
 
-    // load pixel RGB data into 2D array
+    /* Load pixel RGB data into 2D array */
     int pixel_matrix_red[imageWidth][imageHeight];
     int pixel_matrix_green[imageWidth][imageHeight];
     int pixel_matrix_blue[imageWidth][imageHeight];
 
-    //cout << "Iterating through pixels: " << endl;
+    cout << "Iterating through pixels... ";
 
     for (int w = 0; w < imageWidth; w++) {
         for (int h = 0; h < imageHeight; h++) {
@@ -40,12 +40,14 @@ int main() {
         }
     }
 
-    // convert RBG tuples to brightness numbers
-    // -- using the average of RGB values
+    cout << "Done!" << endl << endl;
+
+    /* Convert RBG tuples to brightness numbers
+    -- using the average of RGB values */
     int brightness_matrix[imageWidth][imageHeight];
 
     cout << "Brightness matrix size: " << imageWidth << " x " << imageHeight << endl;
-    cout << "Iterating through pixel brightnesses: " << endl;
+    cout << "Iterating through pixel brightnesses... ";
 
     for (int w = 0; w < imageWidth; w++) {
         for (int h = 0; h < imageHeight; h++) {
@@ -55,41 +57,66 @@ int main() {
         }
     }
 
-    // find smallest & largest value in brightness matrix
-    int temp_smallest = 255;
-    int temp_largest = 0;
+    cout << "Done!" << endl << endl;
+
+    /* TEST: find smallest & largest value in brightness matrix
+    int brightness_smallest = 255;
+    int brightness_largest = 0;
     for (int a = 0; a < imageWidth; a++) {
         for (int b = 0; b < imageHeight; b++) {
-            if (brightness_matrix[a][b] < temp_smallest) {
-                temp_smallest = brightness_matrix[a][b];
+            if (brightness_matrix[a][b] < brightness_smallest) {
+                brightness_smallest = brightness_matrix[a][b];
             }
         }
     }
     for (int a = 0; a < imageWidth; a++) {
         for (int b = 0; b < imageHeight; b++) {
-            if (brightness_matrix[a][b] > temp_largest) {
-                temp_largest = brightness_matrix[a][b];
+            if (brightness_matrix[a][b] > brightness_largest) {
+                brightness_largest = brightness_matrix[a][b];
             }
         }
     }
-    cout << "The smallest value in brightness matrix is: " << temp_smallest << endl;
-    cout << "The largest value in brightness matrix is: " << temp_largest << endl;
+    cout << "The smallest value in brightness matrix is: " << brightness_smallest << endl;
+    cout << "The largest value in brightness matrix is: " << brightness_largest << endl; */
 
-
+    /* Convert brightness_matrix to ASCII matrix */
+    char ascii_matrix[imageWidth][imageHeight];
     string ascii_string = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$";
-    int ascii_chars_iteration = 4;
-    for (int s = 0; s < ascii_string.size(); s++) {
+    float brightness_to_ascii_ratio = 4;
+
+    cout << "Size of ASCII string: " << ascii_string.size() << endl;
+
+    //for (int s = 0; s < ascii_string.size(); s++) {
         //cout << ascii_string[s] << endl;
-    }
+    //}
 
-    // convert brightness matrix to ASCII characters
-    for (int w = 0; a < imageWidth; a++) {
-        for (int h = 0; b < imageHeight; b++) {
-
+    for (int w = 0; w < imageWidth; w++) {
+        for (int h = 0; h < imageHeight; h++) {
+            int current_ascii_num = round(brightness_matrix[w][h] / brightness_to_ascii_ratio);
+            ascii_matrix[w][h] = ascii_string[round(brightness_matrix[w][h] / brightness_to_ascii_ratio)];
+            cout << ascii_matrix[w][h] << endl;
         }
     }
 
-
+    /* TEST: find smallest and largest value in ASCII matrix
+    int ascii_smallest = 255;
+    int ascii_largest = 0;
+    for (int a = 0; a < imageWidth; a++) {
+        for (int b = 0; b < imageHeight; b++) {
+            if (ascii_matrix[a][b] < ascii_smallest) {
+                ascii_smallest = ascii_matrix[a][b];
+            }
+        }
+    }
+    for (int a = 0; a < imageWidth; a++) {
+        for (int b = 0; b < imageHeight; b++) {
+            if (ascii_matrix[a][b] > ascii_largest) {
+                ascii_largest = ascii_matrix[a][b];
+            }
+        }
+    }
+    cout << "The smallest value in ASCII matrix is: " << ascii_smallest << endl;
+    cout << "The largest value in ASCII matrix is: " << ascii_largest << endl; */
 
     return 0;
 }
